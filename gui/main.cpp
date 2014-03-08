331a6b40bc117a06be493d8711f1c2a1e97e20eb
graylor <graylor@yandex.ru>
2014-03-09 00:34:57 +0400

    version changed

--- gui/main.cpp
+++ gui/main.cpp
@@ -7,7 +7,7 @@ FastQSPWindow* qspWin;
 int main(int argc, char *argv[])
 {
     QCoreApplication::setApplicationName("FastQSP");
-    QCoreApplication::setApplicationVersion("v0.4.2");
+    QCoreApplication::setApplicationVersion("v0.4.4");
 
     QApplication a(argc, argv);
     qspWin = new FastQSPWindow();
