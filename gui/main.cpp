5665ffb07b40336ce2493d20705e92af180e878b
graylor <graylor@yandex.ru>
2016-02-07 19:39:13 +0300

    Added possibility of cmake build

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,11 +1,14 @@
 #include "fastqspwindow.h"
 #include <QApplication>
 
+#define STRINGIZE_(x) #x
+#define STRINGIZE(x) STRINGIZE_(x)
+
 FastQSPWindow *qspWin;
 
 int main(int argc, char *argv[]) {
   QCoreApplication::setApplicationName("FastQSP");
-  QCoreApplication::setApplicationVersion(GIT_VERSION);
+  QCoreApplication::setApplicationVersion(STRINGIZE(GIT_VERSION));
 
   QApplication a(argc, argv);
   qspWin = new FastQSPWindow();
